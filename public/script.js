5d8388dcb44e7c2ebf7ce34cf05c5ddb82b5fcb6
andersoncarvalhal-dev <andersonpimenta10@hotmail.com>
2026-09-20 17:55:16 -0300

    Adiciona balões de chat estilo WhatsApp

--- public/script.js
+++ public/script.js
@@ -3,9 +3,7 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
 
-
 const username = prompt("Digite seu nome de usuário:") || "Anônimo";
-
 
 socket.emit('join', username);
 
@@ -17,13 +15,17 @@ form.addEventListener('submit', function(e) {
   }
 });
 
-
 socket.on('chat message', function(data) {
   const item = document.createElement('li');
   
   if (data.user === 'Sistema') {
-    item.innerHTML = `<em style="color: gray;">${data.text}</em>`;
+    item.classList.add('message-system');
+    item.innerHTML = `<em>${data.text}</em>`;
+  } else if (data.user === username) {
+    item.classList.add('message-mine');
+    item.innerHTML = `<strong>Você:</strong> ${data.text}`;
   } else {
+    item.classList.add('message-other');
     item.innerHTML = `<strong>${data.user}:</strong> ${data.text}`;
   }
   
